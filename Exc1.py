226806ba68a4cb0522fe07e5dccddd0dcfbc1348
José Carlos <49734983+zecabanhas@users.noreply.github.com>
2019-04-25 21:29:23 -0300

    Exercício 1 estudo dirigido

---
+++ Exc1.py
@@ -0,0 +1,19 @@
+#1. Informe o valor lógico (True ou False) para as proposições lógicas abaixo:
+#a. (True) 2 * 4 == 24/3
+#b. (False) (15 % 4) < (19 % 6)
+#c. (False) !(2 < 5) != (8 == 8)
+#d. (True) True or False
+#e. (True) 2**2 == math.pow(2, 2)
+#f. (False) False and ( (7 / 2) > 3.5 )
+#g. (True) ( math.ceil( 2.5 ) == 3) and (math.sqrt( 9) == 3)
+import math
+
+print (2 * 4 == 24/3)
+print ((15 % 4) < (19 % 6))
+print ((2 < 5) != (8 == 8))
+print (True or False)
+print (2**2 == math.pow(2, 2))
+print (False and ( (7 / 2) > 3.5 ))
+print (( math.ceil( 2.5 ) == 3) and (math.sqrt( 9) == 3))
+
+
